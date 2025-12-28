ad41ea272d019ac0a4db53b460980c00008ec9ef
M-Loads <mattloads@hotmail.co.uk>
2025-12-28 22:28:17 +0100

    Add function to count letters in book text

--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def get_num_words (book_text):
   num_words = len(book_text.split())
   return num_words
+
+def get_letter_count (book_text):
+  lowercase = book_text.lower()
+  letters_dict = {}
+  for x in lowercase:
+    if x in letters_dict:
+      letters_dict[x] = letters_dict[x] + 1
+    else:
+      letters_dict[x] = 1
+  return letters_dict
+
+  
+  

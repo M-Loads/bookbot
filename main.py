b1cb1e5fec4977a712ee27bd5af66ad21556bab6
M-Loads <mattloads@hotmail.co.uk>
2025-12-28 23:28:19 +0100

    Fix function name for letter count calculation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from stats import get_num_words, get_letters_count
 def main():
   text_string = get_book_text ("books/frankenstein.txt")
   word_count = get_num_words(text_string)
-  letter_count = get_letters_count(text_string)
+  letter_count = get_letter_count(text_string)
   print (f"Found {word_count} total words")
   print (letter_count)
 

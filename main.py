0c3650a9a31e1937587476501f1e7b249d8ab99b
M-Loads <mattloads@hotmail.co.uk>
2025-12-28 03:32:37 +0100

    Add word count functionality to main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 def main():
-  result = get_book_text ("books/frankenstein.txt")
-  print (result)
+  text_string = get_book_text ("books/frankenstein.txt")
+  word_count = number_of_words(text_string)
+  print (f"Found {word_count} total words")
 
 def get_book_text(path):
   with open(path) as f:
       book_text = f.read()
 
   return book_text
+
+def number_of_words (book_text):
+  num_words = len(book_text.split())
+
+  return num_words
 
 main()

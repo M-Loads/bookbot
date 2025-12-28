c62afd4e24224a6ccbe6e417451fed1c4ec8423e
M-Loads <mattloads@hotmail.co.uk>
2025-12-28 02:43:39 +0100

    Correct main function definition and call
    Fix syntax errors in main function and call.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def main()
+  result = get_book_text ("books/frankenstein.txt"):
+  print (result)
+
 def get_book_text(path):
   with open(path) as f:
       book_text = f.read()
 
   return book_text
+
+main()

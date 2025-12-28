573b3296ed9b9b0a1f69f2ab287edb11eb4006c7
M-Loads <mattloads@hotmail.co.uk>
2025-12-29 00:46:10 +0100

    Implement sorting of letters by frequency
    Add sorting functionality for letter counts

--- stats.py
+++ stats.py
@@ -12,5 +12,16 @@ def get_letter_count (book_text):
       letters_dict[x] = 1
   return letters_dict
 
-  
+def sort_on(char):
+  return char["num"]
+
+def sort_letters(letters_dict):
+  chars = []
+  for char, count in letters_dict.items():
+    s = {"char": char, "num": count}
+    chars.append(s)
+
+chars.sort(reverse=True, key=sort_on)
+  return chars
+
   

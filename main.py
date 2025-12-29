66d8814d45c9665e1db86bc7bb9de57aef8b06b9
M-Loads <mattloads@hotmail.co.uk>
2025-12-29 01:30:58 +0100

    Integrate letter sorting and reporting in main.py
    Added letter sorting functionality and report printing.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from stats import get_num_words, get_letter_count
+from stats import get_num_words, get_letter_count, sort_letters
 
 def main():
   text_string = get_book_text ("books/frankenstein.txt")
   word_count = get_num_words(text_string)
   letter_count = get_letter_count(text_string)
+  letter_sorting = sort_letters(letter_count)
+  print("============ BOOKBOT ============")
+  print("Analyzing book found at books/frankenstein.txt...")
+  print("----------- Word Count ----------")
   print (f"Found {word_count} total words")
-  print (letter_count)
+  print("--------- Character Count -------")
+  print (sorted_letters)
+  print("============= END ===============")
 
 def get_book_text(path):
   with open(path) as f:
       book_text = f.read()
-
   return book_text
 
+def print_report(sorted_letters):
+  for entry in sorted_letters:
+    char = entry ["char"]
+    num = entry ["num"]
+    if char.isaplha():
+      print (f"{char}: {num}")
 
 main()

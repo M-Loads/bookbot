ccf57747d2d5b1cb6911ed33cf8c829a55d48c6a
M-Loads <mattloads@hotmail.co.uk>
2025-12-29 02:02:56 +0100

    Fix book path input handling in main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
-print ("Usage: python3 main.py <path_to_book>")
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 from stats import get_num_words, get_letter_count, sort_letters
 
 def main():
-  text_string = get_book_text ("books/frankenstein.txt")
+  path = sys.argv[1]
+  text_string = get_book_text (path)
   word_count = get_num_words(text_string)
   letter_count = get_letter_count(text_string)
   letter_sorting = sort_letters(letter_count)
